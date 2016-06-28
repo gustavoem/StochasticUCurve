a75c461b3023a9b4b09181b4acb91275640fcef2
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-06-28 13:14:53 -0500

    Changed block structure to enhance code readability

--- bisection/pmf.py
+++ bisection/pmf.py
@@ -1,11 +1,15 @@
 class Block:
 
-    def __init__ (self, p, size):
+    def __init__ (self, p, start, end):
+        """ The block represents the value of the pmf of the values in the interval 
+        [start, end). """
         self.p = p
-        self.size = size
+        self.start = start
+        self.end = end
 
     def mass (self):
-        return p * size
+        """ Returns the probability mass of the block """
+        return p * (end - start)
 
 
 
@@ -13,7 +17,8 @@ class Block:
 class PMF:
 
     def __init__(self, n):
-        self.blocks = [Block (1.0 / n, n)]
+        """ Creates a PMF represented with blocks with same probability """
+        self.blocks = [Block (1.0 / n, 0, n)]
 
 
     def update (self, mid, direction, pc):
@@ -29,32 +34,55 @@ class PMF:
 
         #print ("preferred side: ", direction)
         #print ("sum before: ", sum (pmf))
+        # calculates alpha and beta
+        mid_block = __find_mid_block (mid)
+        alpha = __calculate_alpha (mid)
+        beta = alpha - mid_block.p
 
-        # calculates alpha
- 
+        if (direction < 0):
+            if (1 - beta < 1e-8):
+                return
+
+
+
+            
+    def __find_mid_block (self, mid):
+        """ Finds the block that contains the mid element probability """
+        block_i = 0
+        while (self.blocks[block_i].end < mid):
+            block_i += 1
+        return block_i
+        
+
+    def __split_in (self, i, block_i):
+        """ Splits the block block_i, which contains the element i in two blocks. The i
+        element will be in the last element of the first block """
+
+
+
 
     def calculate_alpha (self, mid):
+        """ Calculates alpha and beta where:
+            alpha = P (i* <= mid) """
         alpha = 0
         block_i = 0
-        element_i = 0
-        print ("pmf: ", self.toString ())
 
-        while (element_i + self.blocks[block_i].size < mid):
+        while (self.blocks[block_i].end < mid):
             alpha += self.blocks[block_i].mass
-            element_i += self.blocks[block_i].size
             block_i += 1
 
-        alpha += self.blocks[block_i].p * (mid - element_i)
+        block_start = self.blocks[block_i].start
+        alpha += self.blocks[block_i].p * (mid - block_start + 1)
         return alpha
 
 
     def toString (self):
         s = ""
         for block in self.blocks:
-            s += str (block.p) + ": " + str (block.size) + "; "
+            s += "-| " + str (block.p) + ": " + str (block.start) + "-" + str (block.end) + " |-"
         return s
 
 
 pmf = PMF (7)
-alpha = pmf.calculate_alpha (4)
+alpha = pmf.calculate_alpha (1)
 print (alpha)
